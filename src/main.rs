563600da14f6f0537915889f9b5cba389c5ff2ea
nbari <nbari@tequila.io>
2019-04-11 23:28:29 +0200

    sync Thu Apr 11 23:28:29 CEST 2019

--- src/main.rs
+++
@@ -1,8 +0,0 @@
-use std::{thread, time};
-
-fn main() {
-    loop {
-        println!("Loop forever!");
-        thread::sleep(time::Duration::new(2, 0));
-    }
-}
---
+++ src/slack.rs
@@ -0,0 +1,25 @@
+extern crate slack_hook;
+
+use self::slack_hook::{Slack, PayloadBuilder};
+use std::env;
+use std::process;
+
+pub fn send_msg() {
+    let slack_url = env::var("SLACK_WEBHOOK_URL").unwrap_or_else(|e| {
+        println!("could not find {}: {}", "SLACK_WEBHOOK_URL", e);
+        process::exit(1);
+    });
+    let slack = Slack::new(&*slack_url).unwrap();
+    let p = PayloadBuilder::new()
+        .text("test message")
+        .channel("#noisy-neighbours")
+        .username("dbpulse")
+        .icon_emoji(":warning:")
+        .build()
+        .unwrap();
+    let res = slack.send(&p);
+    match res {
+        Ok(()) => println!("ok"),
+        Err(x) => println!("ERR: {:?}",x)
+    }
+}
