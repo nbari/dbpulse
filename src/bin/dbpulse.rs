3b24704ba30d9411e90f665e06575256ec75def8
nbari <nbari@tequila.io>
2019-05-23 16:28:54 +0200

    sync Thu May 23 16:28:54 CEST 2019

--- src/bin/dbpulse.rs
+++ src/bin/dbpulse.rs
@@ -6,21 +6,45 @@ use std::{thread, time};
 use tokio::prelude::*;
 use tokio::timer::Interval;
 
+use futures::future::join_all;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
+
 fn main() {
+    let locker = Arc::new(AtomicBool::new(false));
+
     let task = Interval::new(time::Instant::now(), time::Duration::new(1, 0))
-        .for_each(|interval| {
-            println!("Interval: {:?}", interval);
-            for i in 0..5 {
-                tokio::spawn(lazy(move || {
-                    println!("Hello from task {}", i);
-                    // mock delay
-                    thread::sleep(time::Duration::from_secs(3));
+        .map_err(|e| panic!("interval errored; err={:?}", e))
+        .for_each(move |interval| {
+            let is_locked = locker.load(Ordering::SeqCst);
+            println!("Interval: {:?} --- {:?}", interval, is_locked);
+
+            if !is_locked {
+                locker.store(true, Ordering::SeqCst);
+                println!("locked");
+
+                let futures: Vec<_> = (0..5)
+                    .map(|i| {
+                        lazy(move || {
+                            println!("Hello from task {}", i);
+                            // mock delay
+                            thread::sleep(time::Duration::from_secs(5));
+                            Ok(())
+                        })
+                    })
+                .collect();
+
+                let unlocker = locker.clone();
+                tokio::spawn(join_all(futures).and_then(move |_| {
+                    unlocker.store(false, Ordering::SeqCst);
+                    println!("unlocked");
+
                     Ok(())
                 }));
             }
-            Ok(())
-        })
-    .map_err(|e| panic!("interval errored; err={:?}", e));
 
-    tokio::run(task);
+            Ok(())
+        });
+
+    tokio::run(task.then(|_| Ok(())));
 }
