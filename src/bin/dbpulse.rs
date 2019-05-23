9da69927fcbdebe85de12dd50feee316c5850d66
nbari <nbari@tequila.io>
2019-05-23 19:22:36 +0200

    sync Thu May 23 19:22:36 CEST 2019

--- src/bin/dbpulse.rs
+++ src/bin/dbpulse.rs
@@ -1,50 +1,46 @@
 extern crate tokio;
 extern crate futures;
 
-use futures::future::lazy;
-use std::{thread, time};
-use tokio::prelude::*;
-use tokio::timer::Interval;
-
-use futures::future::join_all;
-use std::sync::atomic::{AtomicBool, Ordering};
-use std::sync::Arc;
+use futures::future::{self, Loop}; // 0.1.26
+use std::time::{Duration, Instant};
+use tokio::{prelude::*, timer::Delay};  // 0.1.18
 
 fn main() {
-    let locker = Arc::new(AtomicBool::new(false));
+    let repeat_count = Some(5);
 
-    let task = Interval::new(time::Instant::now(), time::Duration::new(1, 0))
-        .map_err(|e| panic!("interval errored; err={:?}", e))
-        .for_each(move |interval| {
-            let is_locked = locker.load(Ordering::SeqCst);
-            println!("Interval: {:?} --- {:?}", interval, is_locked);
+    let forever = future::loop_fn(repeat_count, |repeat_count| {
+        eprintln!("Loop starting at {:?}", Instant::now());
 
-            if !is_locked {
-                locker.store(true, Ordering::SeqCst);
-                println!("locked");
+        // Resolves when all pages are done
+        let batch_of_pages = future::join_all(all_pages());
 
-                let futures: Vec<_> = (0..5)
-                    .map(|i| {
-                        lazy(move || {
-                            println!("Hello from task {}", i);
-                            // mock delay
-                            thread::sleep(time::Duration::from_secs(5));
-                            Ok(())
-                        })
-                    })
-                .collect();
+        // Resolves when both all pages and a delay of 1 second is done
+        let wait = Future::join(batch_of_pages, ez_delay_ms(1000));
 
-                let unlocker = locker.clone();
-                tokio::spawn(join_all(futures).and_then(move |_| {
-                    unlocker.store(false, Ordering::SeqCst);
-                    println!("unlocked");
-
-                    Ok(())
-                }));
+        // Run all this again
+        wait.map(move |_| {
+            if let Some(0) = repeat_count {
+                Loop::Break(())
+            } else {
+                Loop::Continue(repeat_count.map(|c| c - 1))
             }
+        })
+    });
 
-            Ok(())
-        });
+    tokio::run(forever.map_err(drop));
+}
 
-    tokio::run(task.then(|_| Ok(())));
+fn all_pages() -> Vec<Box<dyn Future<Item = (), Error = ()> + Send + 'static>> {
+    vec![Box::new(page("a", 1000)), Box::new(page("b", 2000)), Box::new(page("c", 3000))]
+}
+
+fn page(name: &'static str, time_ms: u64) -> impl Future<Item = (), Error = ()> + Send + 'static {
+    future::ok(())
+        .inspect(move |_| eprintln!("page {} starting", name))
+        .and_then(move |_| ez_delay_ms(time_ms))
+        .inspect(move |_| eprintln!("page {} done", name))
+}
+
+fn ez_delay_ms(ms: u64) -> impl Future<Item = (), Error = ()> + Send + 'static {
+    Delay::new(Instant::now() + Duration::from_millis(ms)).map_err(drop)
 }
