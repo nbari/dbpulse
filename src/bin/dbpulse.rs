af6fa6386d6d612fdb87a08f90f6fd70d833810b
nbari <nbari@tequila.io>
2019-05-27 22:56:24 +0200

    sync Mon May 27 22:56:24 CEST 2019

--- src/bin/dbpulse.rs
+++ src/bin/dbpulse.rs
@@ -1,5 +1,3 @@
-extern crate dbpulse;
-
 //use dbpulse::slack;
 use std::{
     thread,
@@ -12,43 +10,32 @@ fn main() {
     loop {
         let wait_time = Duration::from_secs(30);
         let start = Instant::now();
-        println!("Scheduler starting at {:?}", start);
 
-        scheduler();
+        tasks();
 
         let runtime = start.elapsed();
         if let Some(remaining) = wait_time.checked_sub(runtime) {
-            println!(
-                "schedule slice has time left over; sleeping for {:?}",
-                remaining
-            );
             thread::sleep(remaining);
         }
-        //        slack::send_msg();
     }
 }
 
-fn scheduler() {
-    let scheduler = thread::spawn(|| {
+fn tasks() {
+    let task = thread::spawn(|| {
         let mut children = vec![];
 
-        for i in 0..30 {
-            children.push(thread::spawn(move || {
-                println!("this is thread number {}", i);
-                mock_delay();
-            }));
-        }
+        children.push(thread::spawn(move || {
+            not_sleeping();
+        }));
 
         for child in children {
             // Wait for the thread to finish. Returns a result.
             let _ = child.join();
         }
-
     });
-
-    scheduler.join().expect("Scheduler panicked");
+    task.join().expect("task panicked");
 }
 
-fn mock_delay() {
+fn not_sleeping() {
     Command::new("sleep").arg("3").output().expect("failed to execute process");
 }
