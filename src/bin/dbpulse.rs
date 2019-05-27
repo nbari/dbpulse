7c5e13cad4b99317d509f5f83bd66382fc06c5a1
nbari <nbari@tequila.io>
2019-05-27 15:20:38 +0200

    sync Mon May 27 15:20:38 CEST 2019

--- src/bin/dbpulse.rs
+++ src/bin/dbpulse.rs
@@ -1,45 +1,59 @@
+extern crate dbpulse;
+extern crate rand;
+
+use dbpulse::slack;
 use std::{
     thread,
     time::{Duration, Instant},
     process::Command,
 };
 
+use rand::Rng;
+
+
 fn main() {
     loop {
-        let scheduler = thread::spawn(|| {
-            let wait_time = Duration::from_secs(5);
-            let start = Instant::now();
-            println!("Scheduler starting at {:?}", start);
+        let wait_time = Duration::from_secs(10);
+        let start = Instant::now();
+        println!("Scheduler starting at {:?}", start);
 
-            let mut children = vec![];
+        scheduler();
 
-            for i in 0..5 {
-                children.push(thread::spawn(move || {
-                    println!("this is thread number {}", i);
-                    mock_delay();
-                }));
-            }
-
-            for child in children {
-                // Wait for the thread to finish. Returns a result.
-                let _ = child.join();
-            }
-
-            let runtime = start.elapsed();
-
-            if let Some(remaining) = wait_time.checked_sub(runtime) {
-                println!(
-                    "schedule slice has time left over; sleeping for {:?}",
-                    remaining
-                );
-                thread::sleep(remaining);
-            }
-        });
-
-        scheduler.join().expect("Scheduler panicked");
+        let runtime = start.elapsed();
+        if let Some(remaining) = wait_time.checked_sub(runtime) {
+            println!(
+                "schedule slice has time left over; sleeping for {:?}",
+                remaining
+            );
+            thread::sleep(remaining);
+        }
+        slack::send_msg();
     }
 }
 
+fn scheduler() {
+    let scheduler = thread::spawn(|| {
+        let mut children = vec![];
+
+        for i in 0..30 {
+            children.push(thread::spawn(move || {
+                println!("this is thread number {}", i);
+                mock_delay();
+            }));
+        }
+
+        for child in children {
+            // Wait for the thread to finish. Returns a result.
+            let _ = child.join();
+        }
+
+    });
+
+    scheduler.join().expect("Scheduler panicked");
+}
+
 fn mock_delay() {
-    Command::new("sleep").arg("3").output().expect("failed to execute process");
+    let num = rand::thread_rng().gen_range(1, 5);
+    println!("sleeping for {}", num);
+    Command::new("sleep").arg(format!("{}", num)).output().expect("failed to execute process");
 }
