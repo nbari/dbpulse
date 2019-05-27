c45c6d80c2744ab2e38a9ca57015071cbcd912bc
nbari <nbari@tequila.io>
2019-05-27 12:43:15 +0200

    sync Mon May 27 12:43:15 CEST 2019

--- src/bin/dbpulse.rs
+++ src/bin/dbpulse.rs
@@ -1,54 +1,45 @@
-extern crate futures;
-extern crate tokio;
-
-use futures::future::lazy;
-use std::time::{self, Duration, Instant};
-
-use tokio::prelude::*;
-use tokio::timer::{Delay, Interval};
-
-use futures::future::join_all;
-use std::sync::atomic::{AtomicBool, Ordering};
-use std::sync::Arc;
+use std::{
+    thread,
+    time::{Duration, Instant},
+    process::Command,
+};
 
 fn main() {
-    let locker = Arc::new(AtomicBool::new(false));
+    loop {
+        let scheduler = thread::spawn(|| {
+            let wait_time = Duration::from_secs(5);
+            let start = Instant::now();
+            println!("Scheduler starting at {:?}", start);
 
-    let task = Interval::new(time::Instant::now(), time::Duration::new(1, 0))
-        .map_err(|e| panic!("interval errored; err={:?}", e))
-        .for_each(move |interval| {
-            let is_locked = locker.load(Ordering::SeqCst);
-            println!("Interval: {:?} --- {:?}", interval, is_locked);
+            let mut children = vec![];
 
-            if !is_locked {
-                locker.store(true, Ordering::SeqCst);
-                println!("locked");
-
-                let futures: Vec<_> = (0..1)
-                    .map(|i| {
-                        lazy(move || {
-                            println!("Running Task-{} in  Thread {:?}", i, std::thread::current().id());
-                            // mock delay
-                            Ok(())
-                        })
-                        .and_then(move |_| {
-                            println!("Task-{} is done in  Thread {:?}", i, std::thread::current().id());
-                            Ok(())
-                        })
-                    })
-                .collect();
-
-                let unlocker = locker.clone();
-                tokio::spawn(join_all(futures).and_then(move |_| {
-                    unlocker.store(false, Ordering::SeqCst);
-                    println!("unlocked\n\n");
-
-                    Ok(())
+            for i in 0..5 {
+                children.push(thread::spawn(move || {
+                    println!("this is thread number {}", i);
+                    mock_delay();
                 }));
             }
 
-            Ok(())
+            for child in children {
+                // Wait for the thread to finish. Returns a result.
+                let _ = child.join();
+            }
+
+            let runtime = start.elapsed();
+
+            if let Some(remaining) = wait_time.checked_sub(runtime) {
+                println!(
+                    "schedule slice has time left over; sleeping for {:?}",
+                    remaining
+                );
+                thread::sleep(remaining);
+            }
         });
 
-    tokio::run(task.then(|_| Ok(())));
+        scheduler.join().expect("Scheduler panicked");
+    }
+}
+
+fn mock_delay() {
+    Command::new("sleep").arg("3").output().expect("failed to execute process");
 }
